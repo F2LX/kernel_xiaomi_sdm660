0fbc94e508e45060c1e8324a9b4b0b37365efd89
psndna88 <psndna88@gmail.com>
2020-04-03 13:45:41 +0530

    disable tracing/logging at various places Part 7
    Signed-off-by: Parvinder Singh <psndna88@gmail.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1056,7 +1056,7 @@ static int ext4_nfs_commit_metadata(struct inode *inode)
 		.sync_mode = WB_SYNC_ALL
 	};
 
-	trace_ext4_nfs_commit_metadata(inode);
+//	trace_ext4_nfs_commit_metadata(inode);
 	return ext4_write_inode(inode, &wbc);
 }
 
