e3558828cfa26cdfa5f95cfd708f85677a45ec16
F2LX <felixfebryan16@gmail.com>
2020-06-28 00:00:27 +0000

    Compiling Script Update

--- build-all.sh
+++ build-all.sh
@@ -1,27 +1,13 @@
-echo -e "Compile Started for all devices"
 source build-whyred.sh
-cd out
-rclone copy *.zip drive:whyred
 cd ..
 source build-whyred-new.sh
-cd out
-rclone copy *.zip drive:whyred
 cd ..
 source build-tulip.sh
-cd out
-rclone copy *.zip drive:tulip
 cd ..
 source build-tulip-new.sh
-cd out
-rclone copy *.zip drive:tulip
 cd ..
 source build-wayne.sh
-cd out
-rclone copy *.zip drive:wayne
 cd ..
 source build-wayne-new.sh
-cd out
-rclone copy *.zip drive:wayne
 cd ..
-echo -e "All files uploaded"
-echo -e "Done, K thx"
+echo -e "Build Uploaded Successfully"
--- build-whyred.sh
+++ build-whyred.sh
@@ -15,21 +15,24 @@ BUILD_START=$(date +"%s")
 KERNEL_DIR=$PWD
 REPACK_DIR=$KERNEL_DIR/AnyKernel3
 OUT=$KERNEL_DIR/out
-VERSION="nightly"
-
-export ARCH=arm64 && SUBARCH=arm64
-export CROSS_COMPILE="/home/felix/aospgcc/bin/aarch64-linux-android-"
+VERSION="5.0"
 
 rm -rf out
 mkdir -p out
 make O=out clean
 make O=out mrproper
 make O=out ARCH=arm64 whyred_defconfig
-make -j$(nproc --all) O=out
+PATH="/space/felix/clang/clang-r383902c/bin:/space/felix/gcc/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+ARCH=arm64 \
+CC="ccache /space/felix/clang/clang-r383902c/bin/clang" \
+CLANG_TRIPLE=aarch64-linux-gnu- \
+CROSS_COMPILE=aarch64-linux-android- \
+CROSS_COMPILE_ARM32="/space/felix/gcc32/bin/arm-linux-androideabi-"
 
 cd $REPACK_DIR
 cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb $REPACK_DIR/
-FINAL_ZIP="PhantomCAF-whyred-oldcam-${VERSION}.zip"
+FINAL_ZIP="PhantomCAF-EAS-oldcam-${VERSION}.zip"
 zip -r9 "${FINAL_ZIP}" *
 cp *.zip $OUT
 rm *.zip
@@ -39,4 +42,6 @@ rm AnyKernel3/Image.gz-dtb
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo -e "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+cd out
+rclone copy *.zip drive:whyred
 echo -e "Done"
