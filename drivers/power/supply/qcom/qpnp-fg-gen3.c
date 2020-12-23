59dcd5378bfd24d5173fca649e1ac1239fe6e75d
F2LX <felixfebryan16@gmail.com>
2020-12-23 03:11:42 +0000

    Revert "power: add a simple api to query the charging state (on/off) at any point in time"
    This reverts commit c8dc4f56c06d85cfeb9c73569a2704fd2227dc7b.

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -24,7 +24,6 @@
 #include <linux/thermal.h>
 #include "fg-core.h"
 #include "fg-reg.h"
-#include <linux/charging_state.h>
 
 #define FG_GEN3_DEV_NAME	"qcom,fg-gen3"
 
@@ -407,13 +406,6 @@ static int fg_sram_dump_period_ms = 20000;
 module_param_named(
 	sram_dump_period_ms, fg_sram_dump_period_ms, int, S_IRUSR | S_IWUSR
 );
-
-bool is_charging = false;
-
-bool charging_detected(void)
-{
-	return is_charging;
-}
 
 static int fg_restart;
 static bool fg_sram_dump;
@@ -1711,14 +1703,12 @@ static void fg_cap_learning_update(struct fg_chip *chip)
 	/* Initialize the starting point of learning capacity */
 	if (!chip->cl.active) {
 		if (chip->charge_status == POWER_SUPPLY_STATUS_CHARGING) {
-			is_charging = true;
 			rc = fg_cap_learning_begin(chip, batt_soc);
 			chip->cl.active = (rc == 0);
 		} else {
 			if ((chip->charge_status ==
 					POWER_SUPPLY_STATUS_DISCHARGING) ||
 					chip->charge_done)
-				is_charging = false;
 				prime_cc = true;
 		}
 	} else {
