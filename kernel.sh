0a150ae88e91cee37c87d817df6f6c732ff0819f
F2LX <felixfebryan16@gmail.com>
2020-12-26 02:35:19 +0000

    scripts: update compiler directory

--- kernel.sh
+++ kernel.sh
@@ -21,10 +21,10 @@ mkdir -p out
 make O=out clean
 make O=out mrproper
 make O=out ARCH=arm64 ${DEVICE_NAME}_defconfig
-PATH="/users/felix/proton/bin:${PATH}" \
+PATH="/mnt/ssd/workspace/felix/proton/bin:${PATH}" \
 make -j$(nproc --all) O=out \
 ARCH=arm64 \
-CC="ccache /users/felix/proton/bin/clang" \
+CC="ccache /mnt/ssd/workspace/felix/proton/bin/clang" \
 CROSS_COMPILE=aarch64-linux-gnu- \
 CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
