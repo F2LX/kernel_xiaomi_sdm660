3e3b64afe3426656482cca9ed0a4a23f4e21ccd8
Tabah6 <74711146+Tabah6@users.noreply.github.com>
2021-03-11 23:09:45 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,60 @@
 #!/bin/bash
-# Usage: Build.sh <Version>
 
-VERSION=$1
+export KERNELNAME=Tabah
 
-. kernel.sh whyred ${VERSION}
-cd ..
-. kernel.sh whyred-new ${VERSION}
-cd ..
-. kernel.sh tulip ${VERSION}
-cd ..
-. kernel.sh tulip-new ${VERSION}
-cd ..
-. kernel.sh a26x ${VERSION}
-cd ..
-. kernel.sh a26x-new ${VERSION}
-cd ..
-. kernel.sh a26x-miuiq ${VERSION}
-cd ..
-. kernel.sh lavender ${VERSION}
-cd ..
-. kernel.sh lavender-new ${VERSION}
-cd ..
+export LOCALVERSION=ILove-V1.0
 
+export KBUILD_BUILD_USER=Tabah
+
+export KBUILD_BUILD_HOST=Sdn2Tunahan
+
+export TOOLCHAIN=clang
+
+export DEVICES=whyred,tulip,lavender,a26x
+
+export CI_ID=-1001420838318
+
+export GROUP_ID=-1001328102824
+
+source helper
+
+gen_toolchain
+
+send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+
+send_pesan "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+
+START=$(date +"%s")
+
+for i in ${DEVICES//,/ }
+do
+	build ${i} -oldcam
+
+	build ${i} -newcam
+done
+
+send_msg "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+
+send_pesan "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+
+git apply oc.patch
+
+git apply em.patch
+
+for i in ${DEVICES//,/ }
+do
+	if [ $i == "whyred" ] || [ $i == "tulip" ]
+	then
+		build ${i} -oldcam -overclock
+
+		build ${i} -newcam -overclock
+	fi
+done
+
+END=$(date +"%s")
+
+DIFF=$(( END - START ))
+
+send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
+
+send_pesan "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
