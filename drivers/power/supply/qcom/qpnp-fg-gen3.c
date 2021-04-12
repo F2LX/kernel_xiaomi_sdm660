276b1a62c24f6b97b0f0904036ce9e67e9c7dce4
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-04-12 14:28:22 +0700

    drivers/power: apply changes from merge caf 'LA.UM.8.2.r2-03800-sdm660.0'
    this following apply commit:
    - 6db0f9ed49635991c7d1bab4032671f1780e9b1c ("iio: qcom-rradc: Update logic to monitor health of RRADC peripheral")
    - e5225ad6133325e375e191aa0cc767fcdb94a4de ("qcom: fg-memif: Correct timeout condition for memory grant")
    - d81e600c586eefde2fe173ac4e6638dabf6c5500 ("power: qpnp-fg-gen3: Add a property to reset FG BCL device")
    
    also this following revert commit:
    - abcb76f9ac3a28b01fb8977cd9c0b84a8a01d280 ("drivers/power: some revert from merge caf 'LA.UM.8.2.r2-03800-sdm660.0'") with keep some changes from Xiaomi
    
    Xiaomi changes:
    - https://github.com/okta-10/mystic_kernel_sdm660-4.4/commit/998058d9c6f4a5f1322dd27dd90738517372f935#diff-ece3bf375efd5f4e855ca6177101343f2092e516fad21a92cda9dffb0cb50699R424
    - https://github.com/okta-10/mystic_kernel_sdm660-4.4/commit/998058d9c6f4a5f1322dd27dd90738517372f935#diff-ece3bf375efd5f4e855ca6177101343f2092e516fad21a92cda9dffb0cb50699R468
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -4075,11 +4075,9 @@ static int fg_psy_get_property(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_CC_STEP_SEL:
 		pval->intval = chip->ttf.cc_step.sel;
 		break;
-#if defined(CONFIG_KERNEL_CUSTOM_E7T)
 	case POWER_SUPPLY_PROP_FG_RESET_CLOCK:
 		pval->intval = 0;
 		break;
-#endif
 	default:
 		pr_err("unsupported property %d\n", psp);
 		rc = -EINVAL;
@@ -4094,21 +4092,20 @@ static int fg_psy_get_property(struct power_supply *psy,
 
 	return 0;
 }
-#if defined(CONFIG_KERNEL_CUSTOM_E7T)
 #define BCL_RESET_RETRY_COUNT 4
 static int fg_bcl_reset(struct fg_chip *chip)
 {
 	int i, ret, rc = 0;
 	u8 val, peek_mux;
 	bool success = false;
-	pr_err("FG_BCL_RESET START\n");
+
 	/* Read initial value of peek mux1 */
 	rc = fg_read(chip, BATT_INFO_PEEK_MUX1(chip), &peek_mux, 1);
 	if (rc < 0) {
 		pr_err("Error in writing peek mux1, rc=%d\n", rc);
 		return rc;
 	}
-	pr_err("FG_BCL_RESET PEEK_MUX = %d\n",peek_mux);
+
 	val = 0x83;
 	rc = fg_write(chip, BATT_INFO_PEEK_MUX1(chip), &val, 1);
 	if (rc < 0) {
@@ -4118,7 +4115,6 @@ static int fg_bcl_reset(struct fg_chip *chip)
 
 	mutex_lock(&chip->sram_rw_lock);
 	for (i = 0; i < BCL_RESET_RETRY_COUNT; i++) {
-		pr_err("FG_BCL_RESET RETRY\n");
 		rc = fg_dma_mem_req(chip, true);
 		if (rc < 0) {
 			pr_err("Error in locking memory, rc=%d\n", rc);
@@ -4130,9 +4126,8 @@ static int fg_bcl_reset(struct fg_chip *chip)
 			pr_err("Error in reading rdback, rc=%d\n", rc);
 			goto release_mem;
 		}
-		pr_err("FG_BCL_RESET VAL = %d\n",val);
+
 		if (val & PEEK_MUX1_BIT) {
-			pr_err("FG_BCL_RESET DEBUG\n");
 			rc = fg_masked_write(chip, BATT_SOC_RST_CTRL0(chip),
 						BCL_RESET_BIT, BCL_RESET_BIT);
 			if (rc < 0) {
@@ -4143,7 +4138,8 @@ static int fg_bcl_reset(struct fg_chip *chip)
 
 			rc = fg_dma_mem_req(chip, false);
 			if (rc < 0)
-				pr_err("Error in unlocking memory, rc=%d\n", rc);
+				pr_err("Error in unlocking memory, rc=%d\n",
+						rc);
 
 			/* Delay of 2ms */
 			usleep_range(2000, 3000);
@@ -4159,8 +4155,9 @@ static int fg_bcl_reset(struct fg_chip *chip)
 		} else {
 			rc = fg_dma_mem_req(chip, false);
 			if (rc < 0) {
-				pr_err("Error in unlocking memory, rc=%d\n", rc);
-				return rc;
+				pr_err("Error in unlocking memory, rc=%d\n",
+						rc);
+				goto unlock;
 			}
 			success = false;
 			pr_err_ratelimited("PEEK_MUX1 not set retrying...\n");
@@ -4188,7 +4185,7 @@ unlock:
 	else
 		return rc;
 }
-#endif
+
 static int fg_psy_set_property(struct power_supply *psy,
 				  enum power_supply_property psp,
 				  const union power_supply_propval *pval)
@@ -4240,15 +4237,6 @@ static int fg_psy_set_property(struct power_supply *psy,
 			return -EINVAL;
 		}
 		break;
-#if defined(CONFIG_KERNEL_CUSTOM_E7T)
-	case POWER_SUPPLY_PROP_FG_RESET_CLOCK:
-		rc = fg_bcl_reset(chip);
-		if (rc < 0) {
-			pr_err("Error in resetting BCL clock, rc=%d\n", rc);
-			return rc;
-		}
-		break;
-#endif
 	case POWER_SUPPLY_PROP_CHARGE_FULL:
 		if (chip->cl.active) {
 			pr_warn("Capacity learning active!\n");
@@ -4288,6 +4276,13 @@ static int fg_psy_set_property(struct power_supply *psy,
 		rc = fg_set_jeita_threshold(chip, JEITA_HOT, pval->intval);
 		if (rc < 0) {
 			pr_err("Error in writing jeita_hot, rc=%d\n", rc);
+			return rc;
+		}
+		break;
+	case POWER_SUPPLY_PROP_FG_RESET_CLOCK:
+		rc = fg_bcl_reset(chip);
+		if (rc < 0) {
+			pr_err("Error in resetting BCL clock, rc=%d\n", rc);
 			return rc;
 		}
 		break;
@@ -4392,9 +4387,7 @@ static enum power_supply_property fg_psy_props[] = {
 	POWER_SUPPLY_PROP_CONSTANT_CHARGE_VOLTAGE,
 	POWER_SUPPLY_PROP_CC_STEP,
 	POWER_SUPPLY_PROP_CC_STEP_SEL,
-#if defined(CONFIG_KERNEL_CUSTOM_E7T)
 	POWER_SUPPLY_PROP_FG_RESET_CLOCK,
-#endif
 };
 
 static const struct power_supply_desc fg_psy_desc = {
