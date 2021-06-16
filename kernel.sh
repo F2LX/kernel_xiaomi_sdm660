d9cb0d91feb5483afd7186e450c28786db41bdf5
F2LX <felixfebryan16@gmail.com>
2021-06-17 05:56:25 +0700

    kernel.sh: welcome my new serbur

--- kernel.sh
+++ kernel.sh
@@ -21,10 +21,10 @@ mkdir -p out
 make O=out clean
 make O=out mrproper
 make O=out ARCH=arm64 ${DEVICE_NAME}_defconfig
-PATH="/home/felix/proton/bin:${PATH}" \
+PATH="/home/b/fel/proton/bin:${PATH}" \
 make -j$(nproc --all) O=out \
 ARCH=arm64 \
-CC="ccache /home/felix/proton/bin/clang" \
+CC="ccache /home/b/fel/proton/bin/clang" \
 CROSS_COMPILE=aarch64-linux-gnu- \
 CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
@@ -40,5 +40,5 @@ rm AnyKernel3/Image.gz-dtb
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 cd out
-rclone copy *.zip drive:garudanew
+rclone copy *.zip fel:garudanew
 echo -e "Done"
